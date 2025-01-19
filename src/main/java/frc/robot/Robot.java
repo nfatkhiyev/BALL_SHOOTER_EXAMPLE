8f43519f26972153cf75536d6b7e5c669442405c
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2025-01-18 21:24:58 -0600

    initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+
+  private static final int joystickID = 0;
+  private static final int motorCanID = 9;
+
+  Joystick joystick;
+  SparkMax motor;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    joystick = new Joystick(joystickID);
+    
+    //Create a new Spark Max configuration setting the idle mode to coast.
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.
+      idleMode(IdleMode.kCoast);
+
+    //Create a new motor named motor
+    motor = new SparkMax(motorCanID, MotorType.kBrushless);
+
+    //Apply the configuration to the motor with safe reset parameters and persistent save
+    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    motor.set(joystick.getY());
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
